b3985aea952e0d2438bf6bbe5533b5f2589c9467
dejwkoperfild <d.radziwoniuk@gmail.com>
2026-09-18 19:32:47 +0200

    Początek

---
+++  main.py
@@ -0,0 +1,36 @@
+
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+
+driver = webdriver.Chrome()  
+
+try:
+    # 2. Otwarcie strony docelowej
+    driver.get("http://books.toscrape.com/")
+    
+    
+    time.sleep(2) 
+
+    books_data = []
+    
+    
+    book_containers = driver.find_elements(By.CSS_SELECTOR, "article.product_pod")
+
+   
+    for book in book_containers:
+        title = book.find_element(By.CSS_SELECTOR, "h3 a").get_attribute("title")
+        
+        
+        price = book.find_element(By.CSS_SELECTOR, "p.price_color").text
+        
+        
+        availability = book.find_element(By.CSS_SELECTOR, "p.availability").text.strip()
+        
+        books_data.append([title, price, availability])
+        print(f"Pobrano: {title} | {price}")
+
+finally:
+
+    driver.quit()
